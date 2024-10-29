0bfdf8ed7133e528b7ac74d78df05979dc6858a4
Marcap00 <cappellatomarco@yahoo.com>
2024-10-29 18:30:56 +0100

    added route for Pokemon page show

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'home'])->name('home');
 Route::get('/pokemon', [PokemonController::class, 'index'])->name('pokemon.index');
+Route::get('/pokemon/{id}', [PokemonController::class, 'show'])->name('pokemon.show');
