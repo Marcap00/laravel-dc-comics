806ada54eb218073be32d2099279ae0d202d70a6
Marcap00 <cappellatomarco@yahoo.com>
2024-10-29 16:35:02 +0100

    refactoring css and update home page

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -4,34 +4,7 @@
 @section('main-content')
 <main>
     <div class="container">
-        <div class="row row-cols-4 row-cols-sm-1 row-cols-md-2 g-2">
-            {{-- @forelse ($ as $) --}}
-            <div class="col my-2">
-                <div class="card">
-                    {{-- <a href="{{route('show', $index)}}"> --}}
-                        <div class="card-body">
-                            <h2 class="card-title mb-2">
-                                <i class="fas fa-user me-1"></i>
-                                Lorem
-                            </h2>
-
-                            <p class="card-text">
-                                Lorem<span>Lorem</span>
-                            </p>
-                            <p class="card-text">
-                                Lorem<span>Lorem</span>
-                            </p>
-                            <p class="card-text">
-                                Lorem<span>Lorem</span>
-                            </p>
-                        </div>
-                    {{-- </a> --}}
-                </div>
-            </div>
-            {{-- @empty
-                <p class="text-danger">No more pokemon available...</p>
-            @endforelse --}}
-        </div>
+        <a href="{{route('pokemon.index')}}"><h1>Vai al progetto <i class="fas fa-chevron-right"></i></h1></a>
     </div>
 </main>
 @endsection
---
+++ resources/views/pokemon/index.blade.php
@@ -0,0 +1,37 @@
+@extends('layouts.app')
+@section('head-title', 'Laravel Pokemon Database')
+
+@section('main-content')
+<main>
+    <div class="container">
+        <div class="row row-cols-4 row-cols-sm-1 row-cols-md-2 g-2">
+            {{-- @forelse ($ as $) --}}
+            <div class="col my-2">
+                <div class="card">
+                    {{-- <a href="{{route('show', $index)}}"> --}}
+                        <div class="card-body">
+                            <h2 class="card-title mb-2">
+                                Pokemon
+                            </h2>
+
+                            <p class="card-text">
+                                Lorem: <span>Lorem</span>
+                            </p>
+                            <p class="card-text">
+                                Lorem: <span>Lorem</span>
+                            </p>
+                            <p class="card-text">
+                                Lorem: <span>Lorem</span>
+                            </p>
+                        </div>
+                    {{-- </a> --}}
+                </div>
+            </div>
+            {{-- @empty
+                <p class="text-danger">No more pokemon available...</p>
+            @endforelse --}}
+        </div>
+    </div>
+</main>
+@endsection
+
