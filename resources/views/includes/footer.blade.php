cf33a5ad8820f5daec5e1cf40b35d47dbc8e1de6
Marcap00 <cappellatomarco@yahoo.com>
2024-10-29 21:27:04 +0100

    last refactoring

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,3 +1,3 @@
 <footer>
-    <h5 class="text-danger">Built by MarCap <i class="far fa-copyright fa-xs"></i></h5>
+    <h5 class="text-danger">Created by MarCap <i class="far fa-copyright fa-xs"></i></h5>
 </footer>
--- resources/views/pokemon/show.blade.php
+++ resources/views/pokemon/show.blade.php
@@ -2,38 +2,34 @@
 @section('head-title', 'Laravel Pokemon Database')
 
 @section('main-content')
-<main>
-    <div class="container">
-        <div class="row row-cols-4 row-cols-sm-1 row-cols-md-2 g-2">
-            <div class="col my-2">
-                <div class="card">
-                    <div class="card-img">
-                        <img src="{{ $p->image }}" alt="{{ $p->name }}">
-                    </div>
-                    <div class="card-body">
-                        <h2 class="card-title mb-2">
-                            {{ $p->name }}
-                        </h2>
-                        <p class="card-text">
-                            Category: <span>{{ $p->category }}</span>
-                        </p>
-                        <p class="card-text">
-                            Type: <span>{{ $p->type }}</span>
-                        </p>
-                        <p class="card-text">
-                            Ability: <span>{{ $p->ability }}</span>
-                        </p>
-                        <p class="card-text">
-                            Stage: <span>{{ $p->stage_of_evolution }}° Evolution</span>
-                        </p>
-                        <p class="card-text">
-                            Height: <span>{{ $p->height }}</span>
-                        </p>
-                        <p class="card-text">
-                            Weight: <span>{{ $p->weight }}</span>
-                        </p>
-                    </div>
-                </div>
+<main id="main">
+    <div class="container py-3">
+        <div class="card">
+            <div class="card-img">
+                <img src="{{ $p->image }}" alt="{{ $p->name }}">
+            </div>
+            <div class="card-body">
+                <h2 class="card-title mb-2">
+                    {{ $p->name }}
+                </h2>
+                <p class="card-text">
+                    Category: <span>{{ $p->category }}</span>
+                </p>
+                <p class="card-text">
+                    Type: <span>{{ $p->type }}</span>
+                </p>
+                <p class="card-text">
+                    Ability: <span>{{ $p->ability }}</span>
+                </p>
+                <p class="card-text">
+                    Stage: <span>{{ $p->stage_of_evolution }}° Evolution</span>
+                </p>
+                <p class="card-text">
+                    Height: <span>{{ $p->height }}</span>
+                </p>
+                <p class="card-text">
+                    Weight: <span>{{ $p->weight }}</span>
+                </p>
             </div>
         </div>
     </div>
