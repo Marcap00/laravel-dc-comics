2d2f5d68a2f4cb0cb57f13708efed82b4176e2ea
Marcap00 <cappellatomarco@yahoo.com>
2024-10-29 18:34:18 +0100

    refactoring footer and home

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -4,7 +4,7 @@
 @section('main-content')
 <main>
     <div class="container">
-        <a href="{{route('pokemon.index')}}"><h1>Vai al progetto <i class="fas fa-chevron-right"></i></h1></a>
+        <a href="{{route('pokemon.index')}}"><h1>Go to the project <i class="fas fa-chevron-right"></i></h1></a>
     </div>
 </main>
 @endsection
