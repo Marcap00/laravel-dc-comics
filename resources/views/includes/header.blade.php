4e7deb4a3d5fa74aa9929509b2ff571c228e961d
Marcap00 <cappellatomarco@yahoo.com>
2024-10-29 18:33:35 +0100

    refactoring header

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,4 +1,17 @@
-<header>
-    <img src="{{ asset('logomark.svg') }}" alt="">
-    <h1>Laravel <span>Pokemon</span> Database</h1>
+<header class="flex-between-center">
+    <div class="header-left flex-align-center">
+        <img class="me-2" src="{{ asset('logomark.svg') }}" alt="">
+        <h1>Laravel <span>Pokemon</span> Database</h1>
+    </div>
+    <div class="header right">
+        <ul class="flex-align-center text-white my-3">
+            @foreach ($links_pages as $route=>$text)
+            <li>
+                <a href="{{ route($route) }}">
+                    <h3 class="me-2">{{$text}}</h3>
+                </a>
+            </li>
+            @endforeach
+        </ul>
+    </div>
 </header>
