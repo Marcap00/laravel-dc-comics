64e65be1fb22d9cd8afef47c96bf6010536071ad
Marcap00 <cappellatomarco@yahoo.com>
2024-10-30 17:33:32 +0100

    grouped pokemon routes by prefix and name

--- routes/web.php
+++ routes/web.php
@@ -16,10 +16,23 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [HomeController::class, 'home'])->name('home');
-Route::get('/pokemon', [PokemonController::class, 'index'])->name('pokemon.index');
+
+/* Pokemon Resources */
+
+Route::name('pokemon.')->prefix('pokemon')->group(function () {
+    Route::get('/', [PokemonController::class, 'index'])->name('index');
+    Route::post('/', [PokemonController::class, 'store'])->name('store');
+    Route::get('/create', [PokemonController::class, 'create'])->name('create');
+    Route::get('/{id}', [PokemonController::class, 'show'])->name('show');
+    Route::get('/{id}/edit', [PokemonController::class, 'edit'])->name('edit');
+    Route::put('/{id}', [PokemonController::class, 'update'])->name('update');
+    Route::delete('/{id}', [PokemonController::class, 'destroy'])->name('destroy');
+});
+
+/* Route::get('/pokemon', [PokemonController::class, 'index'])->name('pokemon.index');
 Route::post('/pokemon', [PokemonController::class, 'store'])->name('pokemon.store');
 Route::get('/pokemon/create', [PokemonController::class, 'create'])->name('pokemon.create');
 Route::get('/pokemon/{id}', [PokemonController::class, 'show'])->name('pokemon.show');
 Route::get('/pokemon/{id}/edit', [PokemonCOntroller::class, 'edit'])->name('pokemon.edit');
 Route::put('/pokemon/{id}', [PokemonController::class, 'update'])->name('pokemon.update');
-Route::delete('/pokemon/{id}', [PokemonController::class, 'destroy'])->name('pokemon.destroy');
+Route::delete('/pokemon/{id}', [PokemonController::class, 'destroy'])->name('pokemon.destroy'); */
