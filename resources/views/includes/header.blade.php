6c81988b33e1998821781bedbcc6b91cb9794782
Marcap00 <cappellatomarco@yahoo.com>
2024-10-31 16:13:34 +0100

    add active links

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -3,12 +3,12 @@
         <img class="me-2" src="{{ asset('logomark.svg') }}" alt="">
         <h1>Laravel <span>Pokemon</span> Database</h1>
     </div>
-    <div class="header right">
+    <div class="header-right">
         <ul class="flex-align-center text-white my-3">
             @foreach ($links_pages as $route=>$text)
             <li>
-                <a href="{{ route($route) }}">
-                    <h3 class="me-2">{{$text}}</h3>
+                <a class="{{ (Route::currentRouteName() == $route) ? 'active' : ''}} me-2" href="{{ route($route) }}">
+                    <h3>{{$text}}</h3>
                 </a>
             </li>
             @endforeach
